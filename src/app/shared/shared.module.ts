e161fb88d79edf7d0cf60601eea081601b017985
natelaclaire <nate@natelaclaire.com>
2019-09-26 18:24:07 -0400

    Tweaks to login form

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
- imports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule ],
- exports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule ]
+ imports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule ],
+ exports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule ]
 })
 export class SharedModule { }
