0bb9aadc39ec03c6c9beb97a4419706c67517d05
natelaclaire <nate@natelaclaire.com>
2019-10-10 16:48:58 -0400

    Converted register form to material components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatInputModule} from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
 
 @NgModule({
- imports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule ],
- exports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule ]
+ imports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule ],
+ exports:      [ MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatSelectModule ]
 })
 export class SharedModule { }
